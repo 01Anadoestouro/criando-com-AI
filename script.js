7d0639e136d858530d95e3f3b8046bd233029077
01Anadoestouro <mora.ana@escola.pr.gov.br>
2026-09-01 13:55:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,38 @@ mostraPergunta();const alternativa(const alternativa of perguntaAtual.alternativ
     caixaAlternativas.appendChild(botaoAlternativas);
   }
 }
+botao.addEventListener("click", function () {
+  respostaSelecionada(opcao);
+});botao.addEventListener("click", () => respostaSelecionada(opcao));function respostaSelecionada(opcaoSelecionada) {
+  atual++;
+  mostraPergunta();
+}function respostaSelecionada(opcaoSelecionada) {
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  atual++;
+  mostraPergunta();
+}let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";function respostaSelecionada(opcaoSelecionada) {
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal += afirmacoes + " ";
+  atual++;
+  mostraPergunta();
+}function mostraPergunta() {
+  if (atual >= perguntas.length) {
+    mostraResultado();
+    return;
+  }
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  caixaAlternativas.textContent = "";
+  mostraAlternativas();
+}function mostraResultado() {
+  caixaPerguntas.textContent = "Em 2049...";
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
+}let historiaFinal = "";function respostaSelecionada(opcaoSelecionada) {
+  const afirmacao = opcaoSelecionada.afirmacao;
+  historiaFinal += afirmacao + " ";
+  atual++;
+  mostraPergunta();
+}
